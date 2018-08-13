d5fc037e677657acf475cf63f1e5622fba03d881
Ian Kitchen <ian@oceanrain.com>
2018-08-13 17:48:54 -0400

    Added gallery interaction

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
 	$("#myButton").click(function(){alert("click")});
+		
+	$("#applicationDetails").hide();
+		
+	$("#application").click(function(){
+		//$("#applicationSelectedIndicator").fadeTo("slow", 1);	
+		if( $("#applicationSelectedIndicator").css('opacity') == 1 )
+		{
+			$("#applicationSelectedIndicator").fadeTo("fast", 0);
+		}
+		else
+		{
+			$("#applicationSelectedIndicator").fadeTo("fast", 1);
+		}	
+		
+		$("#applicationDetails").fadeToggle("fast");
+	})
+	
+	
+	
 });
 
 
